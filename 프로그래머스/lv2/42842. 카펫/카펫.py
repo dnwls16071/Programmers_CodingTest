575c2273fc9a8d92c944bb14605c7945867eb8af
dnwls16071 <106802375+dnwls16071@users.noreply.github.com>
2023-07-24 08:39:13 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -2,8 +2,11 @@ def solution(brown, yellow):
     answer = []
     total = brown + yellow
     
+    # 한 변의 길이를 i로 가정
     for i in range(2, total):
+        # 나머지 다른 변의 길이를 total // i로 가정
         side = total // i
+        # 그림과 같이 노란색 블록이 껴있어야 하는 구조이기 때문에 가로, 세로 -2를 처리해준 다음 곱할 때 yellow 개수와 같다면?
         if (i - 2) * (side - 2) == yellow:
             answer = [i, side]
     return answer
