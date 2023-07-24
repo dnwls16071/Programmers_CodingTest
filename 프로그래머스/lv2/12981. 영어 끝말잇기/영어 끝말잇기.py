20ada8f24f3d38c1a37c47d5cbc00819fba2fef2
dnwls16071 <106802375+dnwls16071@users.noreply.github.com>
2023-07-24 11:30:37 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,8 +1,9 @@
 def solution(n, words):
     answer = [0, 0]
     for i in range(1, len(words)):
+        # 해당 단어가 이전에 불렸거나 다음 단어 앞 철자가 이전 단어 마지막 철자와 일치하지 않는 경우
         if words[i] in words[:i] or words[i][0] != words[i-1][-1]:
-            answer[0] = i%n +1
-            answer[1] = i//n +1
+            answer[0] = i % n +1
+            answer[1] = i // n +1
             return answer
     return [0, 0]
