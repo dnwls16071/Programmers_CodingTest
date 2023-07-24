b2736208774dd1ac9e9aed6be1273a2f73ff08e8
dnwls16071 <106802375+dnwls16071@users.noreply.github.com>
2023-07-24 11:51:22 +0900

    Update N개의 최소공배수.py

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -11,3 +11,6 @@ def solution(arr):
         arr = result
     answer = lcm(arr[0], arr[1])
     return answer
+
+# import math를 하면 math.lcm과 math.gcd 모두 사용가능한 것으로 알고 있는데 왜 안되는건지 몰라서 결국 lcm 함수를 직접 선언
+# 두 수의 곱을 최대공약수로 나누면 최소공배수가 나온다.
