bb6faffbdc3c678594d73d6a7c9562a7f5d58f93
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2023-07-25 22:48:53 +0900

    [level 3] Title: 네트워크, Time: 0.16 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,3 +1,20 @@
+# DFS 코드
+def solution(n, computers):
+    def DFS(v):
+        visited[v] = True
+        for i in range(n):
+            if not visited[i] and computers[v][i] == 1:
+                DFS(i)
+    
+    visited = [False] * (n+1)
+    network = 0
+    
+    for i in range(n):
+        if not visited[i]:
+            DFS(i)
+            network += 1
+    return network
+
 # BFS 코드
 from collections import deque
 
@@ -10,9 +27,9 @@ def solution(n, computers):
             
             for i in range(n):
                 if not visited[i] and computers[start][i] == 1:
-                    visited[i] = True
                     queue.append(i)
-        
+                    visited[i] = True
+    
     visited = [False] * (n+1)
     network = 0
     
