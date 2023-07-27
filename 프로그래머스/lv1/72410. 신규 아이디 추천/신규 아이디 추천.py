7912d5ead4b1a9257f51f187aedf257d60aba541
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2023-07-27 20:48:00 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.17 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -33,7 +33,7 @@ def solution(new_id):
     # 만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거
     if len(new_id) >= 16:
         new_id = new_id[:15]
-        if new_id.endswith("."):
+        if new_id[-1] == ".":
             new_id = new_id[:-1]
     
     # 7단계: new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입
