8fefd6ede897f7ead7068ec8ae0d74c44ce1ee26
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2023-07-27 19:51:29 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.py
+++ 프로그래머스/lv0/120875. 평행/평행.py
@@ -2,6 +2,9 @@ def solution(dots):
     # 두 직선이 평행이 된다. → 두 직선의 기울기가 같아야 한다.
     answer = 0
     
+    # 직선의 기울기 값이 항상 정수라고 보장할 수 없다.
+    # 직선의 기울기 값이 소수인 경우도 나올 수 있기 때문에 이는 형변환을 하면 안됨
+    # 따라서 몫을 구하는 연산자인 (//)을 사용하지 않고 실수 형태로 값이 반환되는 (/) 연산자를 사용해야함
     if abs(dots[0][1] - dots[1][1]) / abs(dots[0][0] - dots[1][0]) == abs(dots[2][1] - dots[3][1]) / abs(dots[2][0] - dots[3][0]):
         return 1
     elif abs(dots[0][1] - dots[2][1]) / abs(dots[0][0] - dots[2][0]) == abs(dots[1][1] - dots[3][1]) / abs(dots[1][0] - dots[3][0]):
