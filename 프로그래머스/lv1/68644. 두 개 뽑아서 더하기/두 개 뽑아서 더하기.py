285e449b17494217a44eefaa00d9b20d9f028061
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2023-07-31 00:21:13 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.57 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,3 +1,4 @@
+#1. itertools 모듈과 조합론을 이용한 풀이
 from itertools import permutations
 
 def solution(numbers):
@@ -8,3 +9,16 @@ def solution(numbers):
             answer.append(temp)
     answer.sort()
     return answer
+
+#2. 백트래킹을 이용한 풀이
+def solution(numbers):
+    def backtrack(start, lst):
+        if len(lst) == 2:
+            result.add(sum(lst))
+            return
+        for i in range(start, len(numbers)):
+            backtrack(i + 1, lst + [numbers[i]])
+
+    result = set()
+    backtrack(0, [])
+    return sorted(list(result))
