fb795bc541679cdc02f2e921da0a1f37ded71ed8
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2023-09-17 13:15:43 +0900

    [level 2] Title: 피로도, Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -9,15 +9,15 @@ def solution(k, dungeons):
     
     tot = 0
     for i in res:
-        K = k
-        temp = 0
+        K = k           # 유저의 현재 피로도
+        temp = 0        # 탐험가능한 던전의 갯수
         for j in i:
             # a : 최소 필요 피로도, b : 소모 피로도
             a, b = j[0], j[1]
-            if K >= a and K - b >= 0:
+            if K >= a and K - b >= 0:   # 유저의 현재 피로도가 최소 필요 피로도 이상 & 유저의 현재 피로도에서 소모 피로도를 뺀 값이 0이상
                 K -= b      # 피로도 갱신
-                temp += 1    # 탐험가능한 던전의 숫자
+                temp += 1   
             else:
                 break
-        tot = max(tot, temp)
+        tot = max(tot, temp)    # 최댓값 갱신
     return tot
