6a1fd785823e060b9d8e1107f6c5f3041d18450a
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2023-10-02 21:02:20 +0900

    [level 2] Title: 주식가격, Time: 75.12 ms, Memory: 17.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -0,0 +1,14 @@
+def solution(prices):
+    answer = []
+    for i in range(len(prices)-1):
+        time = 0
+        for j in range(i, len(prices)-1):
+            # 가격이 같거나
+            if prices[i] <= prices[j]:
+                time += 1
+            else:
+                break
+        answer.append(time)
+    # 마지막은 0초간 가격이 떨어지지 않으므로
+    answer.append(0)
+    return answer
